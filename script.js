fa484ae6cb69ae09a840e9a135392ea4ad9603de
Gunay Zahra <126316477+shzehra93@users.noreply.github.com>
2023-08-20 22:14:02 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,45 @@
 (function(){
-    'use strict';
-    document.getElementById("convert").addEventListener("submit", function(event) {
-        event.preventDefault();
-        const distance = parseFloat(document.getElementById("distance").value);
-        const answer = document.getElementById("answer");
+    "use strict";
     
-        if(distance) {
-            const conversion = (distance * 1.60934).toFixed(3);
-            answer.innerHTML = `<h2>${distance} miles is ${conversion} kilometers</h2>`;
-        }
-        else {
-            answer.innerHTML = `<h2>Please enter a number</h2>`;
-        }
+    let convertType = "miles";
+    const heading = document.querySelector("h1");
+    const intro  = document.querySelector("p");
+    const answerDiv = document.querySelector("#answer");
+    const form = document.querySelector("form");
+   
+    document.addEventListener("keydown", function(event){
+       var key = event.code;
+   
+       if (key === "KeyM"){
+           convertType = "miles";
+           heading.textContent = "Miles to Kilometers Converter";
+           intro.textContent = "Type in a number of miles and click the button to convert the distance to kilometers.";
+       }
+       else if (key === "KeyK"){
+           convertType = "kilometers";
+           heading.textContent = "Kilometers to Miles Converter";
+           intro.textContent = "Type in a number of kilometers and click the button to convert the distance to miles.";
+       }
     });
-    
-}) ();
+   
+    form.addEventListener("submit", function(event){
+       event.preventDefault();
+       const distance = parseFloat(document.getElementById('distance').value);
+   
+       if(distance) {
+           if (convertType === "miles") {
+               const converted = (distance * 1.60934).toFixed(3);
+               answerDiv.textContent = `${distance} miles converts to ${converted} kilometers`;
+           }
+           else {
+              const converted = (distance * 0.621371).toFixed(3);
+              answerDiv.textContent = `${distance} kilometers converts to ${converted} miles`;
+           }
+       }
+       else {
+           answerDiv.textContent = "Please enter a number";
+       }
+    });
+   
+   })();
+   
